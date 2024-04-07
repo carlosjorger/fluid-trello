caeb820938813bb9bf8c7fec06f0168e1028761f
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-04-07 10:03:29 -0400

    add TODO list

---
+++ src/components/TodoContainer.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import PlusIcon from "./icons/PlusIcon.vue";
+const todos = ref([] as string[]);
+const currentTodo = ref("");
+const addCart = () => {
+  if (currentTodo.value) {
+    todos.value.push(currentTodo.value);
+  }
+};
+</script>
+<template>
+  <div
+    class="flex gap-2 flex-col p-2 bg-gray-100 border-2 border-solid w-64 border-slate-600 rounded-lg"
+  >
+    <h2>TODOs</h2>
+    <div v-for="todo in todos">
+      <div class="rounded-md shadow-md p-3 bg-gray-50 text-left">
+        {{ todo }}
+      </div>
+    </div>
+    <textarea
+      class="rounded-md shadow-md p-3"
+      type="text"
+      v-model="currentTodo"
+    />
+    <button
+      class="hover:bg-slate-300 flex gap-5 duration-200 transition-colors border-0"
+      v-on:click="addCart"
+    >
+      <plus-icon /> Add a cart
+    </button>
+  </div>
+</template>
