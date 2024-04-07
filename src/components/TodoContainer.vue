1e5e8e9570536aa398d7fc16178e8227b7224b21
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2024-04-07 10:09:40 -0400

    ✨ feat: install vue-flui-dnd

--- src/components/TodoContainer.vue
+++ src/components/TodoContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import PlusIcon from "./icons/PlusIcon.vue";
+import { useDragAndDrop } from "vue-fluid-dnd";
 const todos = ref([] as string[]);
 const currentTodo = ref("");
 const addCart = () => {
@@ -8,17 +9,21 @@ const addCart = () => {
     todos.value.push(currentTodo.value);
   }
 };
+const { parent } = useDragAndDrop(todos);
 </script>
 <template>
   <div
     class="flex gap-2 flex-col p-2 bg-gray-100 border-2 border-solid w-64 border-slate-600 rounded-lg"
   >
     <h2>TODOs</h2>
-    <div v-for="todo in todos">
-      <div class="rounded-md shadow-md p-3 bg-gray-50 text-left">
-        {{ todo }}
+    <div ref="parent" class="flex flex-col gap-1">
+      <div v-for="(todo, index) in todos" :index="index">
+        <div class="rounded-md shadow-md p-3 bg-gray-50 text-left">
+          {{ todo }}
+        </div>
       </div>
     </div>
+
     <textarea
       class="rounded-md shadow-md p-3"
       type="text"
